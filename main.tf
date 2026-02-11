eae59727b73824ddf57060dbd9923917060ca3ca
excalibur1010 <pp8224@g.rit.edu>
2026-02-11 15:55:42 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 # Local variables block for configuration values
 locals {
-    aws_key = "<YOUR-KEY-NAME>"   # SSH key pair name for EC2 instance access
+    aws_key = "pk_key1"   # SSH key pair name for EC2 instance access
 }
 
 # EC2 instance resource definition
@@ -13,9 +13,47 @@ resource "aws_instance" "my_server" {
    ami           = data.aws_ami.amazonlinux.id  # Use the AMI ID from the data source
    instance_type = var.instance_type            # Use the instance type from variables
    key_name      = "${local.aws_key}"          # Specify the SSH key pair name
+   vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
+
+   user_data = file("wp_install.sh")
   
    # Add tags to the EC2 instance for identification
    tags = {
      Name = "my ec2"
    }                  
+}
+# Security group to allow HTTP and SSH access
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http_ssh"
+  description = "Allow HTTP and SSH inbound traffic"
+
+  # Allow SSH (port 22)
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTP (port 80) - THIS IS THE MISSING PIECE FROM ACTIVITY #3!
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http_ssh"
+  }
 }
--- output.tf
+++ output.tf
@@ -1,5 +1,11 @@
-# Output to display the private IP addresses of all created EC2 instances
-# The [*] syntax allows for multiple instances if count or for_each is used
+# Output to display the private IP address of the EC2 instance
 output "instance_ip_addr" {
-  value = aws_instance.my_server[*].private_ip
+  value       = aws_instance.my_server.private_ip
+  description = "The private IP address of the EC2 instance"
+}
+
+# REQUIREMENT #2: Output the public IP address
+output "public_ip" {
+  value       = aws_instance.my_server.public_ip
+  description = "The public IP address of the EC2 instance - use this to access WordPress"
 }
